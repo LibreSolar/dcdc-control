31d6b9adf0b1fe9b862777ecc74797d2fd5157d2
Martin Jaeger <17674105+martinjaeger@users.noreply.github.com>
2019-12-08 13:14:11 +0100

    Proper VDMOS models used for DC/DC

--- model/dcdc_controller.c
+++ model/dcdc_controller.c
@@ -7,8 +7,9 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define DESIRED_VOLTAGE           (30*2.2/100*4096/2.048)
-#define REGULATOR_SCALE_SHIFT   6       /// Kp, and Ki are multiplied by 2^6
+#define DESIRED_VOLTAGE         (30*2.2/100*4096/2.048)
+#define REGULATOR_SCALE_SHIFT   6       // Kp, and Ki are multiplied by 2^6
+#define TIMER_ARR               180     // defines maximum PWM setting
 
 /**
  * Function is called at A/D Sample Rate, and accumulates 64 samples, then
@@ -18,8 +19,8 @@ uint8_t dcdc_controller(uint32_t sample, int32_t Kp, int32_t Ki, uint8_t reset)
 {
     static uint8_t ad_sample_count = 0;
     static int32_t sample_acc = 0;
-    static int32_t integ = 0;
-    static uint8_t pwm = 100;
+    static int32_t integ = (200*12/30 * 2) << REGULATOR_SCALE_SHIFT;      // initialize properly;
+    static uint8_t pwm = 200*12/30;
 
     if (reset) {
         fprintf(stderr, "Firmware Reset\n");
@@ -27,22 +28,18 @@ uint8_t dcdc_controller(uint32_t sample, int32_t Kp, int32_t Ki, uint8_t reset)
         sample_acc = 0;
     }
     else {
-        if (++ad_sample_count > 32) {
+        if (++ad_sample_count > 128) {
             ad_sample_count = 0;
-            sample_acc >>= 5;
+            sample_acc >>= 7;
 
+            // normal PID
             int32_t e = DESIRED_VOLTAGE - sample_acc;
             integ += e * Ki;
             int32_t out = (e * Kp + integ * Ki) >> REGULATOR_SCALE_SHIFT;
-            //pwm = (out > 255) ? 255 : (out < 0) ? 0 : out;
+            //pwm = (out > TIMER_ARR) ? TIMER_ARR : (out < 1) ? 1 : out;
 
-            // simple controller for testing
-            if (sample_acc > DESIRED_VOLTAGE && pwm < 200) {
-                pwm++;
-            }
-            else if (pwm > 1) {
-                pwm--;
-            }
+            // simplified controller
+            pwm += (sample_acc > DESIRED_VOLTAGE) ? 1 : -1;
 
             fprintf(stderr, "I = %d, e = %d, integ = %d, PWM = %d, desired: %d\n", sample_acc, e, integ, pwm, (int)DESIRED_VOLTAGE);
             sample_acc = 0;
